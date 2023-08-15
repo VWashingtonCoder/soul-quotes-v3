6b4d688413d2d16c5438c130d1acfe703a3e53f8
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-08-15 10:35:28 -0600

    Build & style navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  const [page, setPage] = useState("home");
+
+  return (
+    <>
+      <Navbar page={page} changePage={setPage}/>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import logo from "../assets/images/logo.png";
+import { useUser } from "../custom-hooks/useCustomContext";
+
+type NavbarProps = {
+  page: string;
+  changePage: (page: string) => void;
+};
+
+const navLinks = [
+  { key: "home", text: "Home" },
+  { key: "accounts", text: "Account" },
+  { key: "favorites", text: "Favorites" },
+  { key: "create", text: "Create" },
+];
+
+const Navbar = ({ page, changePage }: NavbarProps) => {
+  const { activeUser, logoutActiveUser } = useUser();
+  const username = activeUser?.username || "";
+
+  return (
+    <header className="navbar flex-between-center">
+      <div className="logo-box">
+        <img src={logo} alt="site-logo" className="logo img" />
+      </div>
+
+      <nav className="nav-links flex-between-center">
+        {navLinks.map((link) => {
+          const { key } = link;
+          const btnText = key === "accounts" && username ? "Logout" : link.text;
+
+          return (
+            <button
+              key={key}
+              className={`clear-btn nav-btn ${page === key ? "active" : ""}`}
+              onClick={() => {
+                key === "accounts" && username ? logoutActiveUser() : changePage(key);
+              }}
+            >
+              {btnText}
+            </button>
+          );
+        })}
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
